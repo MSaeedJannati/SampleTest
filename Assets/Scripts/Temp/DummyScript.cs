e755ed7eb814d4cab61ffca02501f48c6b1325c2
SaeedJannati <msaeedjannati72@gmail.com>
2021-03-04 00:49:33 +0330

    DummyCommit

--- Assets/Scripts/Temp/DummyScript.cs
+++ Assets/Scripts/Temp/DummyScript.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class DummyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    #region Variables
+    #endregion
+    #region DummyRegion
+    #endregion
 }
+
