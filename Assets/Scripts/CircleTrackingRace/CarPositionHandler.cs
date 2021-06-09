e23cd87041b10aef37b13b4f305daad0352ebb1d
SaeedJannati <msaeedjannati72@gmail.com>
2021-06-09 18:03:48 +0430

    Circular path race

---
+++ Assets/Scripts/CircleTrackingRace/CarPositionHandler.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CarPositionHandler : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] int carNumber;
+    [SerializeField] Transform mTransform;
+    [SerializeField] float angle;
+    [SerializeField] float currentAngle;
+    [SerializeField] float lastAngle;
+    Vector3 relativePos;
+    float rawAngle;
+    [SerializeField] int lapCount;
+    [SerializeField] Color colour;
+    [SerializeField] TMP_Text carNumText;
+    [SerializeField] SpriteRenderer mRenderer;
+    #endregion
+    #region Properties
+    public float Angle => angle;
+    public int Number { 
+        get => carNumber; 
+    }
+    public Color Colour => colour;
+    #endregion
+    #region monobehaviour callBacks
+    private void Start()
+    {
+        calcAngel();
+        currentAngle = angle;
+        lastAngle = angle;
+        lapCount = 0;
+        colour = mRenderer.color;
+        RaceTrack.AddCar(this);
+    }
+    private void Update()
+    {
+        lastAngle = currentAngle;
+        calcAngel();
+        checkForLapCount();
+        angle = lapCount * 360 + currentAngle;
+    }
+
+    
+    #endregion
+    #region Functions
+    public void calcAngel()
+    {
+        relativePos = mTransform.position - RaceTrack.Center.position;
+        rawAngle = Mathf.Atan(relativePos.y / relativePos.x)*180/Mathf.PI;
+        if (relativePos.x <= 0 && relativePos.y <= 0)
+        {
+            rawAngle = rawAngle + 180;
+        }
+        else if (relativePos.x <= 0 && relativePos.y >= 0)
+        {
+            rawAngle = rawAngle + 180;
+        }
+        else if (relativePos.x >= 0 && relativePos.y <= 0)
+        {
+            rawAngle = rawAngle + 360;
+        }
+        currentAngle = rawAngle;
+    }
+    public void checkForLapIncrease()
+    {
+        if (lastAngle >= 270 && lastAngle < 360)
+        {
+            if (currentAngle >= 0 && currentAngle <= 90)
+            {
+                lapCount++;
+            }
+        }
+    }
+    public void checkForLapDecrease()
+    {
+        if (lastAngle >=0 && lastAngle <= 90)
+        {
+            if (currentAngle >= 270 && currentAngle <= 360)
+            {
+                lapCount--;
+            }
+        }
+    }
+    void checkForLapCount()
+    {
+        checkForLapIncrease();
+        checkForLapDecrease();
+    }
+    public void setCarNumber(int Number)
+    {
+        carNumber = Number;
+        carNumText.text = carNumber.ToString();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/CircleTrackingRace/RaceTrack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using TMPro;
+
+public class RaceTrack : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] Transform centerTransform;
+    static Transform center;
+    static List<CarPositionHandler> cars;
+    string ranks;
+    [SerializeField] TMP_Text leaderBoardText;
+    #endregion
+    #region Propeties
+    public static Transform Center => center;
+    #endregion
+    #region Monobehaviour callBacks
+    private void OnEnable()
+    {
+        cars = new List<CarPositionHandler>();
+        center = centerTransform;
+    }
+    private void Update()
+    {
+        ranks = string.Empty;
+        cars.Sort((y, x) => 
+        {
+           return x.Angle.CompareTo ( y.Angle);
+        }); 
+        for (int i= 0;i < cars.Count;i++)
+        {
+            ranks += $"#{i+1}-Car: {cars[i].Number}\n";
+        }
+
+        leaderBoardText.text = ranks;
+
+    }
+    #endregion
+    #region Functions
+    public static void AddCar(CarPositionHandler car)
+    {
+        cars.Add(car);
+        car.setCarNumber(cars.Count );
+    }
+    #endregion
+}
